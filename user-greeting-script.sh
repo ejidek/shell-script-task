e7ce56c2864f50d9a0f904955c9e792045fcfd70
Ubuntu <ubuntu@ip-172-31-20-69.ec2.internal>
2025-09-02 12:15:54 +0000

    ....

---
+++ user-greeting-script.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#########################
+# Author: Abdulhamid
+# Date: 2nd sep, 25
+# Version: v1
+# 
+# About: This Script expects exactly one argument
+# #######################
+
+# Number of Args expected
+ expected_arg=1
+
+# expected value variable
+ VALUE=$1
+
+# Check if the arg is present
+if [ $# -ne $expected_arg ]; then
+	 echo "❌ Argument must be $expected_arg argument"
+               echo "USAGE: $0 <arg> "
+ else
+	if [[ "$VALUE" =~ ^[0-9]+$ ]]; then
+		echo "❌ Username cannot be Number"
+		echo "USAGE: $0 <Username: sting>"
+                exit 1
+	else
+ 		echo "Welcome $VALUE to Bash Scripting 🎉🎉🎉💯🏅👍"
+		exit 0
+	fi
+fi
+

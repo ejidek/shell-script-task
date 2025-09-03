b5050c130b3a377a56cb9d602b550b5ea4cad0f3
Ubuntu <ubuntu@ip-172-31-20-69.ec2.internal>
2025-09-03 08:41:42 +0000

    ......

--- git-push-script.sh
+++ git-push-script.sh
@@ -13,10 +13,20 @@
 
 ## Add the script to a local repo after passing the arg
  git add $1
-
-## This line will commit the file to the local repo
- git commit -m "......"
  
-## This line will push the file to my git account from the local account
- git push origin main
- git status
+# Check the condition if the argument is pass
+if [ $# -ne 1 ]; then 
+	echo "error, you did'nt pass the require argument"
+	echo "Usage: $0 <arg: Filename>"
+	git status
+	exit 1
+else
+	## This line will commit the file to the local repo
+	git commit -m "......"
+
+	## This line will push the file to my git account from the local account
+	git push origin main
+	git status
+	exit 0
+fi
+

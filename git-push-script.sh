620d89fb48aa0cdf1f6a541f6fe322c8abc89f65
Ubuntu <ubuntu@ip-172-31-20-69.ec2.internal>
2025-09-03 11:05:35 +0000

    git push updated

--- git-push-script.sh
+++ git-push-script.sh
@@ -10,19 +10,21 @@
 #
 ######################################
 
+# Number of args expected
+exp_args=2
 
 ## Add the script to a local repo after passing the arg
  git add $1
  
 # Check the condition if the argument is pass
-if [ $# -ne 1 ]; then 
-	echo "error, you did'nt pass the require argument"
-	echo "Usage: $0 <arg: Filename>"
+if [ $# -ne $exp_args ]; then 
+	echo "error, you did'nt pass the required argument"
+	echo "Usage: $0 <arg: Filename> <arg: comment>"
 	git status
 	exit 1
 else
 	## This line will commit the file to the local repo
-	git commit -m "......"
+	git commit -m "$2"
 
 	## This line will push the file to my git account from the local account
 	git push origin main
